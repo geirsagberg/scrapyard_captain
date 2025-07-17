3049df548413df4a7a2a0ffbc92527085d6d342b
Geir Sagberg <geir.sagberg@gmail.com>
2025-07-17 12:28:35 +0200

    Add tilemap

--- ship.gd
+++ ship.gd
@@ -1,22 +1,12 @@
 extends RigidBody2D
 
-func _process(_delta: float) -> void:
-	var viewport_size = get_viewport_rect().size
-
-	var center = viewport_size / 2
-	var out_of_bounds = position.x < 0 or position.x > viewport_size.x or position.y < 0 or position.y > viewport_size.y
-	if out_of_bounds or Input.is_action_just_pressed("reset_ship"):
-		position = center
-		linear_velocity = Vector2.ZERO
-		angular_velocity = 0.0
-		rotation = 0.0
-		print("Ship reset to center due to out of bounds position.")
+var THRUSTER_FORCE := 10000.0
 
 func _physics_process(delta: float) -> void:
 	var thrusters := get_children().filter(func(child): return child is Thruster and child.enabled)
 	for thruster: Thruster in thrusters:
 		var direction = Vector2.UP.rotated(thruster.global_rotation).snappedf(0.001).normalized()
-		var magnitude = 2000 * delta
+		var magnitude := THRUSTER_FORCE * delta
 		var force = direction * magnitude
 		var force_origin = thruster.position.rotated(rotation)
 		apply_force(force, force_origin)
